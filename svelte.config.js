7a3b5fe038d86e6f2bbaeafb2675810123972f4e
t02uk <tetsuya038@gmail.com>
2023-09-17 18:57:10 +0900

    change svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static';
 
 const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter(),
 		paths: {
