7fac5eb79f3227d18b078b2a260fbdb08f833ba5
t02uk <tetsuya038@gmail.com>
2023-09-17 18:27:15 +0900

    setup deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,17 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.reduce((acc, x) => acc || x == "dev", false)
+
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter()
+  },
+  paths: {
+    base: dev ? '' : '/rrrreplace'
+  }
 }
