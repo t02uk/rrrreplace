66a7a1bd5aca8273c904de92a82ad06440055f72
t02uk <tetsuya038@gmail.com>
2023-09-17 21:44:55 +0900

    add base page in vite.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import adapter from '@sveltejs/adapter-static';
 
-const dev = process.argv.includes('dev');
+// const dev = process.argv.includes('dev');
+const dev = false;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-	kit: {
-		adapter: adapter(),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
-		}
-	}
 };
 export default config;
